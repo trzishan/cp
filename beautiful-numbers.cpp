d06e86e942f3c0a446c19125365e993d38fdd4a2
Tahmid Zishan <tahmidzishan53@gmail.com>
2026-04-17 18:17:50 +0600

    donno

---
+++ beautiful-numbers.cpp
@@ -0,0 +1,23 @@
+// https://codeforces.com/contest/2203/problem/B
+// Author: Tahmid Zishan
+
+#include <iostream>
+using namespace std;
+#define nl '\n'
+
+int main(){
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++){
+		cout << "hello" << nl;
+	}
+	return 0;
+}
+
+// a digit it beautiful if the sum of digits is 
+// equal to the sum of digits of sum of digits
+//
+// f(f(x) == f(x)
+// it is natural when f(x) is single digit
+// to make that happen, we have to eliminate the largest digits first
+//
