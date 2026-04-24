4f75409a4daaa92c50fcbc45137d126c46583ddf
Tahmid Zishan <tahmidzishan53@gmail.com>
2026-04-25 01:43:44 +0600

    session 4

---
+++ class5/name.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	char ch[30];
+	scanf("%[^\n]", ch);
+	printf("%s", ch);
+}
---
+++ integer67.cpp
@@ -0,0 +1,17 @@
+// Author: Tahmid Zishan
+#include <iostream>
+using namespace std;
+
+int main()
+{	
+	int t;
+	cin >> t;
+	for (int i{}; i < t; i++)
+	{
+		int x;
+		cin >> x;
+		cout << x << endl;
+	}
+
+	return 0;
+}
