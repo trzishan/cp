3ad0cc32948894dd135545320b5e0d3ec1906ebe
Tahmid Zishan <tahmidzishan53@gmail.com>
2026-05-07 23:33:51 +0600

    palindrome

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,10 +1,27 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/I
 // Author: Tahmid Zishan
 #include <iostream>
 using namespace std;
 #define nl '\n'
 
+int reverse(int num)
+{	
+	int rev = 0;
+	while (num > 0)
+	{
+		int last = num % 10;
+		rev = rev * 10 + last;
+		num /= 10;
+	}
+	return rev;
+}
+
 int main()
 {
-	
+	int num;
+	cin >> num;
+	int rev = reverse(num);
+	cout << rev << nl;
+	(rev == num) ? cout << "YES" << nl : cout << "NO" << nl;
 	return 0;
 }
